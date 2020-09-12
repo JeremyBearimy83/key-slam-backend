8305060944040bce3cf1563e7ef7674329c25092
Ujjwal Singhal <ujjwals@gmail.com>
2020-09-12 08:29:45 +0530

    5|INST|Install and configure ejs

--- app.js
+++ app.js
@@ -2,8 +2,35 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/user/stats', (req, res) => {
+    res.send('<h1>Here are your stats</h1>');
+})
+
+app.get('/user/race', (req, res) => {
+    res.render('user-race', { user: "Ujjwal Singhal" });
+})
+
+app.get('/user/history', (req, res) => {
+    res.send('<h1>Here is your history</h1>');
+})
+
+app.post('/user/race', (req, res) => {
+    res.send("add stats here");
+})
+
+app.get('/admin/', (req, res) => {
+    res.send("<h1>Welcome Admin</h1>");
+})
+
+app.get('/admin/create-test', (req, res) => {
+    res.send('Create new typing test here');
+})
+
 app.use('/', (req, res) => {
     res.send('<h1>404 page not found</h1>');
 })
 
-app.listen(9000, () => console.log('Listening on port 9000'));
+app.listen(7000, () => console.log('Listening on port 7000'));
